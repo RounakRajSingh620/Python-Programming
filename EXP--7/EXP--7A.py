38b4d779ab75fc82c021cb6e86a13c7e1a108a03
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-03-02 23:32:48 +0530

    Parallel Programming

---
+++ EXP--7/EXP--7A.py
@@ -0,0 +1,19 @@
+import multiprocessing as mp
+import random
+import string
+random.seed(123) 
+output = mp.Queue()
+def rand_string(length, output):
+    rand_str = ''.join(random.choice(
+        string.ascii_lowercase
+        + string.ascii_uppercase
+        + string.digits)
+                       for i in range(length))
+    output.put(rand_str)
+    processes = [mp.Process(target=rand_string, args=(5, output)) for x in range(4)]
+    for p in processes:
+        p.start()
+        for p in processes:
+            p.join()
+            results = [output.get() for p in processes]
+            print(results)
