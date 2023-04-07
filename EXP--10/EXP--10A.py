d2397c5ed7685d4547059a11fafb5e8340e60144
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-07 22:47:03 +0530

    Python Programming

---
+++ EXP--10/EXP--10A.py
@@ -0,0 +1,10 @@
+from pyDatalog import pyDatalog
+pyDatalog.create_terms("A, Pow2, Pow3")
+def square(n):
+    return n*n
+pyDatalog.create_terms("square") 
+input_values = range(10)[::-1]  
+
+(querying with & operator)
+print(A.in_(input_values)&(Pow2==square(A))&(Pow3==A**3))
+
