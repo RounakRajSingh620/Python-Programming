b557d1cd8258356f812e57ee5b2e455a2db2d20e
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-09 23:11:04 +0530

    Python Programming

---
+++ EXP--14/EXP--14A.py
@@ -0,0 +1,27 @@
+import sys
+
+def main():
+    transition = [[[0,1],[0]], [[4],[2]], [[4],[3]], [[4],[4]]]
+    input = raw_input("enter the string: ")
+    input = list(input)
+    for index in range(len(input)): 
+        if input[index]=='a':
+            input[index]='0' 
+        else:
+            input[index]='1'
+
+    final = "3" 
+    i=0  
+    trans(transition, input, final, start , i)
+    print ("rejected")
+def trans(transition, input, final, state, i):
+    for j in range (len(input)):
+        for each in transition[state][int(input[j])]: 
+            if each < 4:                              
+                state = each
+                if j == len(input)-1 and (str(state) in final):
+                    print ("accepted")
+                    sys.exit()
+                trans(transition, input[i+1:], final, state, i)
+        i = i+1 
+main()
