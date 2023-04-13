a2d36d08b0e2dc2c78b80c315d3c909e548e1b11
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-13 22:58:33 +0530

    Python Programming

--- EXP--15/EXP--15A.py
+++ EXP--15/EXP--15A.py
@@ -12,12 +12,10 @@ def btn_equal():
     input_text.set(result)
     expression = " "
     input_text = StringVar()
-input_frame = Frame(window, width=312, height=50, bd=0,
-                    highlightbackground="black", highlightcolor="black", highlightthickness=1)
+input_frame = Frame(window, width=312, height=50, bd=0,highlightbackground="black", highlightcolor="black", highlightthickness=1)
 input_frame.pack(side=TOP)
-# creating a input field inside the 'Frame'
 input_field = Entry(input_frame, font=('arial', 18, 'bold'),
-                    textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT)
+textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT)
 input_field.grid(row=0, column=0)
 input_field.pack(ipady=10)
 # 'ipady' is internal padding to increase the height of input field
