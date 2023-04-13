88ea2a588721704e2350b156550e2f61bf0d7f8a
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-13 22:59:11 +0530

    Python Programming

--- EXP--13/EXP--13A.py
+++ EXP--13/EXP--13A.py
@@ -1,13 +1,14 @@
 from sympy import *
+
 expr = cos(x) + 1
 print(expr.subs(x, y))
 x, y, z = symbols("x y z")
 print(expr=x**y)
 print(expr)
-expr = sin(2*x) + cos(2*x)
+expr = sin(2 * x) + cos(2 * x)
 print(expand_trig(expr))
-print(expr.subs(sin(2*x), 2*sin(x)*cos(x)))
-expr = x**4 - 4*x**3 + 4*x**2 - 2*x + 3
+print(expr.subs(sin(2 * x), 2 * sin(x) * cos(x)))
+expr = x**4 - 4 * x**3 + 4 * x**2 - 2 * x + 3
 replacements = [(x**i, y**i) for i in range(5) if i % 2 == 0]
 print(expr.subs(replacements))
 str_expr = "x**2 + 3*x - 1/2"
@@ -16,7 +17,7 @@ expr
 expr.subs(x, 2)
 expr = sqrt(8)
 print(expr.evalf())
-expr = cos(2*x)
+expr = cos(2 * x)
 expr.evalf(subs={x: 2.4})
-one = cos(1)**2 + sin(1)**2
+one = cos(1) ** 2 + sin(1) ** 2
 print((one - 1).evalf())
