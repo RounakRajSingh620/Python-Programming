74f9108d289cbae4f26217b3e064c96d8090b05f
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-14 19:49:05 +0530

    Python Programming

---
+++ EXP--11/EXP--11B.py
@@ -0,0 +1,13 @@
+from typing import Dict
+import re
+ # Create an alias called 'ContactDict'
+ContactDict = Dict[str, str]
+def check_if_valid(contacts: ContactDict) ->bool:
+    for name, email in contacts.items():
+        if (not isinstance(name, str)) or (not isinstance(email, str)):
+            return False
+        if not re.match(r"[a-zA-Z0-9\._\+-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]+$", email):
+            return False
+    return True
+print(check_if_valid({'viji': 'viji@sample.com'}))
+print(check_if_valid({'viji': 'viji@sample.com', 123: 'wrong@name.com'}))
