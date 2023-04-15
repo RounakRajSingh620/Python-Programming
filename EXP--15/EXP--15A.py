1069bdc8b3f00d1585518155a7c2e3607edc5c80
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-04-15 22:23:57 +0530

    Python Programming

--- EXP--15/EXP--15A.py
+++ EXP--15/EXP--15A.py
@@ -1,33 +1,38 @@
 def btn_click(item):
     global expression
-expression = expression + str(item)
-input_text.set(expression)
+    expression = expression + str(item)
+    input_text.set(expression)
+
+
 def btn_clear():
     global expression
     expression = " "
     input_text.set(" ")
+
+
 def btn_equal():
     global expression
     result = str(eval(expression))
     input_text.set(result)
     expression = " "
-    input_text = StringVar()
-input_frame = Frame(window, width=312, height=50, bd=0,highlightbackground="black", highlightcolor="black", highlightthickness=1)
+    input_text = String()
+
+
+input_frame = frame(window, width=312, height=50, bd=0,
+                    highlightbackground="black", highlightcolor="black", highlightthickness=1)
 input_frame.pack(side=TOP)
 input_field = Entry(input_frame, font=('arial', 18, 'bold'),
-textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT)
+                    textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT)
 input_field.grid(row=0, column=0)
 input_field.pack(ipady=10)
-# 'ipady' is internal padding to increase the height of input field
-# creating another 'Frame' for the button below the 'btns_frame'
 btns_frame = Frame(window, width=312, height=272.5, bg="grey")
 btns_frame.pack()
-# first row
+
 clear = Button(btns_frame, text="C", fg="black", width=32, height=3, bd=0, bg="#eee",
                cursor="hand2", command=lambda: btn_clear()).grid(row=0, column=0, columnspan=3, padx=1, pady=1)
-divide = Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#eee",
+divide = button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#eee",
                 cursor="hand2", command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
-# second row
+
 seven = Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg="#fff",
                cursor="hand2", command=lambda: btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
 eight = Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="#fff",
